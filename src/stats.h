8798dabe77cc2564625884840dbaade90a4573bd
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2024-08-02 21:44:03 +0800

    feat: improve stats report

--- src/stats.h
+++ src/stats.h
@@ -14,7 +14,9 @@ public:
     }
 
     void report(unsigned long long cpu_cycle_count) {
-        fprintf(stderr, "CPU simulatior halted successfully.\n");
+        fprintf(stderr, "CPU simulator halted successfully.\n");
+        fprintf(stderr, "branch count: %llu\n", branch_count);
+        fprintf(stderr, "correct branch predictions: %llu\n", correct_count);
         fprintf(stderr, "branch prediction accuracy: %Lf\n", static_cast<long double>(correct_count) / branch_count);
         fprintf(stderr, "cpu cycle count: %llu\n", cpu_cycle_count);
         fprintf(stderr, "cpu cycle per branch: %Lf\n", static_cast<long double>(cpu_cycle_count) / branch_count);
