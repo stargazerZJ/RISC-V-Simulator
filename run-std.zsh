b1f5692404c6ef5fd51cd94822043e6867ba675b
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2024-08-02 16:37:17 +0800

    test: update the testing script

--- run-std.zsh
+++ run-std.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-executable="cmake-build-debug-g-13-windows/simulator.exe"
+executable="cmake-build-debug-g-13-windows/interpreter.exe"
 input_dir="testcases"
 log_dir="test"
 mkdir -p "$log_dir"
@@ -9,19 +9,19 @@ if [ -n "$1" ]; then
   name=$1
   input_file="$input_dir/$name.data"
 
-  "$executable" < "$input_file" > "$log_dir/$name.out" 2> "$log_dir/$name-sim.err"
+  "$executable" < "$input_file" > "$log_dir/$name.out" 2> "$log_dir/$name-std.err"
   ret_val=$?
 
   if [ $ret_val -eq 0 ]; then
     echo "Execution of $name: Success (Return value: 0)"
-    echo "Error output (saved to $log_dir/$name-sim.err):"
-    tail "$log_dir/$name-sim.err"
+    echo "Error output (saved to $log_dir/$name-std.err):"
+    tail "$log_dir/$name-std.err"
     echo "Output (saved to $log_dir/$name.out):"
     cat "$log_dir/$name.out"
   else
     echo "Execution of $name: Failure (Return value: $ret_val)"
-    echo "Error output (saved to $log_dir/$name-sim.err):"
-    tail "$log_dir/$name-sim.err"
+    echo "Error output (saved to $log_dir/$name-std.err):"
+    tail "$log_dir/$name-std.err"
 fi
 
 fi
